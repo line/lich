4ff550efc391c7465915923e7364cca2deeb28da
Daniel Vila <dtvc87@hotmail.com>
2019-11-07 17:53:43 +0900

    Add OptionalArgumentRequiredDetector
    The detector checks whether a viewmodel's required argument is annotated as optional.

---
+++ static-analysis/src/main/java/com/example/static_analysis/LichIssueRegistry.kt
@@ -0,0 +1,25 @@
+package com.example.static_analysis
+
+import com.android.tools.lint.client.api.IssueRegistry
+import com.android.tools.lint.detector.api.CURRENT_API
+import com.android.tools.lint.detector.api.Issue
+import com.example.static_analysis.detectors.OptionalArgumentRequiredDetector
+import com.google.auto.service.AutoService
+
+
+/**
+ * A class to register custom [Issue]s
+ */
+@AutoService(IssueRegistry::class)
+class LichIssueRegistry : IssueRegistry() {
+    override val issues: List<Issue>
+        get() = listOf(OptionalArgumentRequiredDetector.ISSUE)
+
+    override val api: Int = CURRENT_API
+
+    override val minApi: Int = MIN_API
+
+    companion object {
+        const val MIN_API = 2 // Corresponds to android gradle plugin 3.2+
+    }
+}
---
+++ static-analysis/src/main/java/com/example/static_analysis/detectors/OptionalArgumentRequiredDetector.kt
@@ -0,0 +1,74 @@
+package com.example.static_analysis.detectors
+
+import com.android.tools.lint.detector.api.Category
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.Severity
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import com.android.tools.lint.detector.api.isKotlin
+import com.example.static_analysis.extensions.evaluateOrDefault
+import com.example.static_analysis.extensions.findClosestParentByType
+import com.intellij.psi.PsiMethod
+import org.jetbrains.uast.UCallExpression
+import org.jetbrains.uast.UField
+
+/**
+ * Detector which checks whether a viewmodel's required argument is annotated as optional.
+ */
+class OptionalArgumentRequiredDetector : Detector(), SourceCodeScanner {
+    companion object {
+        @JvmField
+        val ISSUE: Issue = Issue.create(
+            "ArgumentInitializationInconsistency",
+            "Argument inconsistency: the argument is annotated as optional but it is " +
+                "initialized as required.",
+            "An argument annotated as optional can not be initialized as `required`. Remove the " +
+                "annotation or provide another initialization mechanism like `initial`.",
+            Category.CORRECTNESS,
+            6,
+            Severity.ERROR,
+            Implementation(OptionalArgumentRequiredDetector::class.java, Scope.JAVA_FILE_SCOPE)
+        )
+
+        private const val argumentAnnotationQualifiedName = "com.linecorp.lich.viewmodel.Argument"
+        private const val savedStateQualifiedName = "com.linecorp.lich.viewmodel.SavedState"
+    }
+
+    private fun JavaContext.report(node: UCallExpression) {
+        report(
+            ISSUE,
+            node,
+            getNameLocation(node),
+            "Optional argument initialized as required."
+        )
+    }
+
+    /**
+     * Returns true when the [node] is annotated as a optional argument.
+     */
+    private fun isOptionalArgument(node: UCallExpression): Boolean {
+        val field = node.findClosestParentByType<UField>() ?: return false
+        val argumentAnnotation = field.annotations.find {
+            it.qualifiedName == argumentAnnotationQualifiedName
+        } ?: return false
+        val isOptionalAttribute =
+            argumentAnnotation.attributeValues.find { it.name == "isOptional" } ?: return false
+
+        return isOptionalAttribute.expression.evaluateOrDefault(false)
+    }
+
+    override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
+        if (isKotlin(method) &&
+            context.evaluator.isMemberInClass(method, savedStateQualifiedName)
+        ) {
+            if (isOptionalArgument(node)) {
+                context.report(node)
+            }
+        }
+    }
+
+    override fun getApplicableMethodNames(): List<String>? = listOf("required")
+}
