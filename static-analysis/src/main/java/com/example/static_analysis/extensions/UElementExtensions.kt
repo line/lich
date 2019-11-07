8008a1c4eacaa0442f7ab07780dc231eef4edea6
Daniel Vila <dtvc87@hotmail.com>
2019-11-07 17:52:12 +0900

    Add extension functions to simplify logic
    The extensions simply finding parents of a certain type from a source node, as well as, evaluating expressions to a certain type or returning a default value.

---
+++ static-analysis/src/main/java/com/example/static_analysis/extensions/UElementExtensions.kt
@@ -0,0 +1,33 @@
+package com.example.static_analysis.extensions
+
+import org.jetbrains.uast.UElement
+import org.jetbrains.uast.UExpression
+
+/**
+ * By default finds the closest parent of a certain type to this node. When an [ancestorLevel] is
+ * provided it will keep going up on the tree until the parent matches the ancestor level and the
+ * type.
+ *
+ * @param ancestorLevel when provided it will go up in the tree until it matches the ancestor level.
+ */
+inline fun <reified T : UElement> UElement?.findClosestParentByType(ancestorLevel: Int = 1): T? {
+    var node: UElement? = this?.uastParent
+    var currentAncestorLevel = 0
+    while (node != null) {
+        if (node is T) {
+            currentAncestorLevel++
+        }
+        if (currentAncestorLevel == ancestorLevel) {
+            return node as? T
+        }
+        node = node.uastParent
+    }
+    return null
+}
+
+/**
+ * Evaluates this [UExpression] and returns its value. Returns [defaultValue] when the
+ * [UExpression] couldn't be evaluated.
+ */
+inline fun <reified T> UExpression.evaluateOrDefault(defaultValue: T): T =
+    evaluate() as? T ?: defaultValue
