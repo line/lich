d9d313193499262a31fb646e5b35a33169f6029b
OISHI Masakuni <yamasa@bsdhouse.org>
2019-12-20 15:57:25 +0900

    Set default default User-Agent header.

--- sample_app/src/main/java/com/linecorp/lich/sample/GlobalOkHttpClient.kt
+++ sample_app/src/main/java/com/linecorp/lich/sample/GlobalOkHttpClient.kt
@@ -16,8 +16,12 @@
 package com.linecorp.lich.sample
 
 import android.content.Context
+import android.os.Build
 import com.linecorp.lich.component.ComponentFactory
+import okhttp3.Interceptor
+import okhttp3.Interceptor.Chain
 import okhttp3.OkHttpClient
+import okhttp3.Response
 
 /**
  * The application-wide singleton of [OkHttpClient].
@@ -47,10 +51,31 @@ import okhttp3.OkHttpClient
  * ```
  */
 object GlobalOkHttpClient : ComponentFactory<OkHttpClient>() {
-    override fun createComponent(context: Context): OkHttpClient {
-        val builder = OkHttpClient.Builder()
-        //// Apply custom settings.
-        //builder.addInterceptor(LoggingInterceptor())
-        return builder.build()
+    override fun createComponent(context: Context): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(UserAgentInterceptor(buildUserAgent()))
+            .build()
+
+    /**
+     * Builds the default User-Agent value.
+     */
+    private fun buildUserAgent(): String =
+        "LichSample/${BuildConfig.VERSION_NAME} (Android ${Build.VERSION.RELEASE}; ${Build.MODEL})"
+
+    /**
+     * An [Interceptor] that sets the default User-Agent header unless it is explicitly specified in
+     * Request.
+     */
+    private class UserAgentInterceptor(private val userAgent: String) : Interceptor {
+        override fun intercept(chain: Chain): Response {
+            val originalRequest = chain.request()
+            if (originalRequest.header("User-Agent") != null) {
+                return chain.proceed(originalRequest)
+            }
+            val newRequest = originalRequest.newBuilder()
+                .addHeader("User-Agent", userAgent)
+                .build()
+            return chain.proceed(newRequest)
+        }
     }
 }
