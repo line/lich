9b84572f6e2279f21b824b631bd2a9b499b60975
daniel.vila <dtvc87@hotmail.com>
2020-01-29 17:08:37 +0900

    Review fix
    - Reorganize imports.
    - Remove minApi
    - Initialize list of issues statically.

--- static-analysis/src/main/java/com/linecorp/lich/static_analysis/LichIssueRegistry.kt
+++ static-analysis/src/main/java/com/linecorp/lich/static_analysis/LichIssueRegistry.kt
@@ -3,27 +3,20 @@ package com.linecorp.lich.static_analysis
 import com.android.tools.lint.client.api.IssueRegistry
 import com.android.tools.lint.detector.api.CURRENT_API
 import com.android.tools.lint.detector.api.Issue
-import com.linecorp.lich.static_analysis.detectors.OptionalArgumentRequiredDetector
 import com.google.auto.service.AutoService
 import com.linecorp.lich.static_analysis.detectors.LichFactoryDetector
+import com.linecorp.lich.static_analysis.detectors.OptionalArgumentRequiredDetector
 
 /**
  * A class to register custom [Issue]s
  */
 @AutoService(IssueRegistry::class)
 class LichIssueRegistry : IssueRegistry() {
-    override val issues: List<Issue>
-        get() = listOf(
-            OptionalArgumentRequiredDetector.ISSUE,
-            LichFactoryDetector.TYPE_ARGUMENT_ISSUE,
-            LichFactoryDetector.OBJECT_ISSUE
-        )
+    override val issues: List<Issue> = listOf(
+        OptionalArgumentRequiredDetector.ISSUE,
+        LichFactoryDetector.TYPE_ARGUMENT_ISSUE,
+        LichFactoryDetector.OBJECT_ISSUE
+    )
 
     override val api: Int = CURRENT_API
-
-    override val minApi: Int = MIN_API
-
-    companion object {
-        const val MIN_API = 2 // Corresponds to android gradle plugin 3.2+
-    }
 }
