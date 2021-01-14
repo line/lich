b4a847ff6148c092b82e7f08edc8195233ce67bc
OISHI Masakuni <yamasa@bsdhouse.org>
2021-01-14 18:07:52 +0900

    Add extensions for Range Requests defined in RFC 7233.

---
+++ okhttp/src/main/java/com/linecorp/lich/okhttp/ResumableDownload.kt
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linecorp.lich.okhttp
+
+import com.linecorp.lich.okhttp.ContentRange.Companion.mayGetSinglePartContentRange
+import com.linecorp.lich.okhttp.ContentRange.Companion.mayGetTotalLengthOfUnsatisfiedRange
+import okhttp3.Request
+import okhttp3.Response
+import okio.Sink
+import java.io.IOException
+
+/**
+ * Saves the response body to the given [WritableResource] with handling a single-part partial
+ * response defined in [RFC 7233](https://tools.ietf.org/html/rfc7233).
+ *
+ * @param resourceToSave the resource to save the downloaded content.
+ * @return `true` if the download of the entire content is completed. `false` if the response is
+ * `206 Partial Content` and there may be remaining undownloaded content.
+ * @throws InconsistentContentRangeException if the response code is `206 Partial Content` or
+ * `416 Range Not Satisfiable`, and the `Content-Range` header value does not match the length of
+ * [resourceToSave].
+ * @throws ResponseStatusException if the response code is neither `200 OK` nor
+ * `206 Partial Content` nor `416 Range Not Satisfiable`.
+ * @throws IOException if any other I/O error occurs.
+ */
+fun Response.saveToResourceWithSupportingResumption(resourceToSave: WritableResource): Boolean {
+    val sink: Sink
+    val isLastPart: Boolean
+
+    when (code()) {
+        StatusCode.OK -> {
+            // The response body contains the entire content.
+            sink = resourceToSave.newSink(append = false)
+            isLastPart = true
+        }
+        StatusCode.PARTIAL_CONTENT -> {
+            val contentRange = mayGetSinglePartContentRange()
+                ?: throw InconsistentContentRangeException("No valid Content-Range header in the `Partial Content` response.")
+            if (contentRange.start != resourceToSave.length) {
+                throw InconsistentContentRangeException("The start offset of the partial response does not match the length of the resource.")
+            }
+            // The response body contains the continuation of the resource.
+            sink = resourceToSave.newSink(append = true)
+            isLastPart = contentRange.isLastPart
+        }
+        StatusCode.RANGE_NOT_SATISFIABLE -> {
+            val totalLength = mayGetTotalLengthOfUnsatisfiedRange()
+                ?: throw InconsistentContentRangeException("No unsatisfied Content-Range header in the `Range Not Satisfiable` response.")
+            if (totalLength != resourceToSave.length) {
+                throw InconsistentContentRangeException("The content length of the `Range Not Satisfiable` response does not match the length of the resource.")
+            }
+            // The entire content was already downloaded to the resource.
+            return true
+        }
+        else -> throw ResponseStatusException(code())
+    }
+
+    sink.use { checkNotNull(body()).source().readAll(it) }
+
+    return isLastPart
+}
+
+/**
+ * Sets a single-part `Range` header with the given [start] offset.
+ *
+ * Specification: [RFC 7233, section 3.1](https://tools.ietf.org/html/rfc7233#section-3.1)
+ */
+fun Request.Builder.setRangeHeader(start: Long): Request.Builder {
+    require(start >= 0) { "The start position must not be less than zero." }
+    return header("Range", "bytes=$start-")
+}
+
+/**
+ * Sets a single-part `Range` header with the given [start] and [endInclusive] offset.
+ *
+ * Specification: [RFC 7233, section 3.1](https://tools.ietf.org/html/rfc7233#section-3.1)
+ */
+fun Request.Builder.setRangeHeader(start: Long, endInclusive: Long): Request.Builder {
+    require(start >= 0) { "The start position must not be less than zero." }
+    require(endInclusive >= start) { "The end position must not be less than the start position." }
+    return header("Range", "bytes=$start-$endInclusive")
+}
+
+/**
+ * Sets a single-part `Range` header with the given [suffixLength].
+ *
+ * Specification: [RFC 7233, section 3.1](https://tools.ietf.org/html/rfc7233#section-3.1)
+ */
+fun Request.Builder.setSuffixRangeHeader(suffixLength: Long): Request.Builder {
+    require(suffixLength > 0) { "The suffix length must be greater than zero." }
+    return header("Range", "bytes=-$suffixLength")
+}
---
+++ okhttp/src/main/java/com/linecorp/lich/okhttp/WritableResource.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linecorp.lich.okhttp
+
+import okio.Okio
+import okio.Sink
+import java.io.File
+
+/**
+ * A resource that supports writing to it.
+ */
+interface WritableResource {
+    /**
+     * The byte length of this resource.
+     * If the substance for this resource does not exist, this value is `0`.
+     */
+    val length: Long
+
+    /**
+     * Opens a new [Sink] that writes to this resource.
+     * If the substance for this resource does not exist, it will be created.
+     *
+     * @param append If `true`, data will be written to the end of the resource rather than
+     * the beginning.
+     */
+    fun newSink(append: Boolean): Sink
+
+    companion object {
+        /**
+         * Returns a [WritableResource] that represents the given File.
+         */
+        fun File.asWritableResource(): WritableResource = FileWritableResource(this)
+
+        private class FileWritableResource(private val file: File) : WritableResource {
+            override val length: Long
+                get() = file.length()
+
+            override fun newSink(append: Boolean): Sink =
+                if (append) Okio.appendingSink(file) else Okio.sink(file)
+        }
+    }
+}
