08620c7f7af1da84492e3514789c9c5d99b45d6b
OISHI Masakuni <yamasa@bsdhouse.org>
2021-01-25 15:10:05 +0900

    Make callWithCounting() handle partial responses defined in RFC 7233.

--- okhttp/src/main/java/com/linecorp/lich/okhttp/CallWithCounting.kt
+++ okhttp/src/main/java/com/linecorp/lich/okhttp/CallWithCounting.kt
@@ -15,6 +15,8 @@
  */
 package com.linecorp.lich.okhttp
 
+import com.linecorp.lich.okhttp.ContentRange.Companion.mayGetSinglePartContentRange
+import com.linecorp.lich.okhttp.ContentRange.Companion.mayGetTotalLengthOfUnsatisfiedRange
 import kotlinx.coroutines.channels.SendChannel
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.delay
@@ -85,17 +87,21 @@ import java.io.IOException
  * @param request An OkHttp [Request] to be executed.
  * @param countUpload If `false`, don't count the number of bytes in the request body.
  * @param countDownload If `false`, don't count the number of bytes in the response body.
- * @param throttleMillis Limit the emission of [CallState.Uploading] or [CallState.Downloading] to
+ * @param handlePartialResponse If `true`, includes the `Content-Range` header value of a partial
+ * response in [CallState.Downloading] and [CallState.Success].
+ * @param throttleMillis Limits the emission of [CallState.Uploading] or [CallState.Downloading] to
  * at most once every millisecond of this value.
- * @param responseHandler a function to process an OkHttp [Response]. The response object will be
+ * @param responseHandler A function to process an OkHttp [Response]. The response object will be
  * closed automatically after the function call. This function is called from a background thread of
  * OkHttp's thread pool.
  * @return A [Flow] that emits the progress of the HTTP call.
+ * @see Response.saveToResourceWithSupportingResumption
  */
 fun <T> OkHttpClient.callWithCounting(
     request: Request,
     countUpload: Boolean = true,
     countDownload: Boolean = true,
+    handlePartialResponse: Boolean = true,
     throttleMillis: Long = 100L,
     responseHandler: (Response) -> T
 ): Flow<CallState<T>> {
@@ -103,7 +109,15 @@ fun <T> OkHttpClient.callWithCounting(
         val countingRequestBody =
             request.takeIf { countUpload }?.body()?.let { CountingRequestBody(it, channel) }
         val call = newCall(countingRequestBody?.injectTo(request) ?: request)
-        call.enqueue(CountingCallback(countingRequestBody, countDownload, responseHandler, channel))
+        call.enqueue(
+            CountingCallback(
+                countingRequestBody,
+                countDownload,
+                handlePartialResponse,
+                responseHandler,
+                channel
+            )
+        )
         awaitClose(call::cancel)
     }.conflate()
     return if (throttleMillis > 0) {
@@ -122,20 +136,18 @@ sealed class CallState<out T> {
      */
     interface Progress {
         /**
-         * The number of bytes that have been sent / received as an HTTP request / response body
-         * so far.
+         * The number of bytes that have been sent / received so far.
          */
         val bytesTransferred: Long
 
         /**
-         * The number of bytes of the request / response body specified in the `Content-Length`
-         * header. If the header does not exist, this value is `-1`.
+         * The number of bytes of the entire content. If it is unknown, this value is `-1`.
          */
         val bytesTotal: Long
 
         /**
-         * Percentage of transferred bytes to entire request / response body. (`0..100`)
-         * If the `Content-Length` header does not exist, this is `null`.
+         * Percentage of transferred bytes to the entire content. (`0..100`)
+         * If [bytesTotal] is unknown, this value is `null`.
          */
         val progressPercentage: Int?
             get() = when {
@@ -147,15 +159,28 @@ sealed class CallState<out T> {
 
     /**
      * An intermediate state indicating that the HTTP request body is being sent.
-     * This state is not emitted if the `countUpload` parameter is `false` or the `request` has no
-     * body.
+     *
+     * This state is not emitted if the `countUpload` parameter is `false` or the `request` has no body.
+     *
+     * @property bytesTransferred The number of bytes that have been sent as the request body so far.
+     * @property bytesTotal The length of the request body specified in the `Content-Length` header.
      */
     class Uploading(override val bytesTransferred: Long, override val bytesTotal: Long) :
         CallState<Nothing>(), Progress
 
     /**
      * An intermediate state indicating that the HTTP response body is being received.
-     * This state is not emitted if the `countDownload` parameter is `false`.
+     *
+     * This state is not emitted if the `countDownload` parameter is `false` or the response status
+     * code is other than `2xx`.
+     *
+     * @property bytesTransferred The number of bytes that have been sent as the response body so far.
+     * If the `handlePartialResponse` parameter is `true` and the response is a single-part
+     * `206 Partial Content` response, this value includes the start offset of the `Content-Range` header.
+     * @property bytesTotal If the `handlePartialResponse` parameter is `true` and the response is
+     * a single-part `206 Partial Content` response, this value is the total length of the
+     * `Content-Range` header. Otherwise, this is the length of the response body specified in the
+     * `Content-Length` header.
      */
     class Downloading(override val bytesTransferred: Long, override val bytesTotal: Long) :
         CallState<Nothing>(), Progress
@@ -164,11 +189,16 @@ sealed class CallState<out T> {
      * A final state indicating that the HTTP call completed successfully.
      * After emitting this state, the `flow` completes normally.
      *
-     * @param data The result returned by the `responseHandler`.
-     * @param bytesUploaded The number of bytes finally sent as the HTTP request body,
+     * @property data The result returned by the `responseHandler`.
+     * @property bytesUploaded The number of bytes finally sent as the request body,
      * or `-1` if the `countUpload` parameter is `false` or the `request` has no body.
-     * @param bytesDownloaded The number of bytes finally received as the HTTP response body,
-     * or `-1` if the `countDownload` parameter is `false`.
+     * @property bytesDownloaded The number of bytes finally received as the response body, unless
+     * the `countDownload` parameter is `false` or the response status code is other than `2xx`.
+     * If the `handlePartialResponse` parameter is `true` and the response is a single-part
+     * `206 Partial Content` response, this value includes the start offset of the `Content-Range` header.
+     * If the `handlePartialResponse` parameter is `true` and the response is `416 Range Not Satisfiable`,
+     * this value is the total length of the `Content-Range` header.
+     * If neither of them, this value is `-1`.
      */
     class Success<T>(val data: T, val bytesUploaded: Long, val bytesDownloaded: Long) :
         CallState<T>()
@@ -179,7 +209,7 @@ sealed class CallState<out T> {
      *
      * This state is also emitted when the `responseHandler` throws an [IOException].
      *
-     * @param exception the [IOException] that caused the failure.
+     * @property exception the [IOException] that caused the failure.
      */
     class Failure(val exception: IOException) : CallState<Nothing>()
 }
@@ -247,11 +277,12 @@ private class CountingSink(
  */
 private class CountingSource(
     delegate: Source,
+    startOffset: Long,
     private val contentLength: Long,
     private val channel: SendChannel<CallState.Downloading>
 ) : ForwardingSource(delegate) {
 
-    var bytesDownloaded: Long = 0
+    var bytesDownloaded: Long = startOffset
 
     override fun read(sink: Buffer, byteCount: Long): Long =
         super.read(sink, byteCount).also { bytesRead ->
@@ -278,6 +309,7 @@ private class CountingSource(
 private class CountingCallback<T>(
     private val countingRequestBody: CountingRequestBody?,
     private val countDownload: Boolean,
+    private val handlePartialResponse: Boolean,
     private val responseHandler: (Response) -> T,
     private val channel: SendChannel<CallState<T>>
 ) : Callback {
@@ -285,7 +317,7 @@ private class CountingCallback<T>(
     override fun onResponse(call: Call, response: Response) {
         runAndCloseChannel {
             val finalState = try {
-                processResponse(response)
+                response.use { processResponse(it) }
             } catch (e: IOException) {
                 CallState.Failure(e)
             }
@@ -303,30 +335,45 @@ private class CountingCallback<T>(
         }
     }
 
-    private fun processResponse(originalResponse: Response): CallState.Success<T> {
-        val countingSource: CountingSource?
-        val responseForHandler: Response
-        if (countDownload) {
-            // Inject `CountingSource` to the response body.
-            val originalBody = checkNotNull(originalResponse.body())
-            countingSource =
-                CountingSource(originalBody.source(), originalBody.contentLength(), channel)
+    private fun processResponse(response: Response): CallState.Success<T> {
+        val countingSource = mayCreateCountingSource(response)
+        val responseForHandler = if (countingSource != null) {
+            val originalBody = checkNotNull(response.body())
             val countingResponseBody = ResponseBody.create(
                 originalBody.contentType(),
                 originalBody.contentLength(),
                 Okio.buffer(countingSource)
             )
-            responseForHandler = originalResponse.newBuilder().body(countingResponseBody).build()
-        } else {
-            countingSource = null
-            responseForHandler = originalResponse
-        }
+            response.newBuilder().body(countingResponseBody).build()
+        } else response
 
-        val data = responseForHandler.use { responseHandler(it) }
+        val data = responseHandler(responseForHandler)
 
         val bytesUploaded = countingRequestBody?.bytesUploaded ?: -1
-        val bytesDownloaded = countingSource?.bytesDownloaded ?: -1
+        val bytesDownloaded = when {
+            countingSource != null -> countingSource.bytesDownloaded
+            countDownload && handlePartialResponse &&
+                response.code() == StatusCode.RANGE_NOT_SATISFIABLE ->
+                response.mayGetTotalLengthOfUnsatisfiedRange() ?: -1
+            else -> -1
+        }
         return CallState.Success(data, bytesUploaded, bytesDownloaded)
+    }
+
+    private fun mayCreateCountingSource(response: Response): CountingSource? {
+        if (!countDownload || !response.isSuccessful) return null
+
+        val originalBody = checkNotNull(response.body())
+        val originalSource = originalBody.source()
+        val contentRange =
+            if (handlePartialResponse && response.code() == StatusCode.PARTIAL_CONTENT) {
+                response.mayGetSinglePartContentRange()
+            } else null
+        return if (contentRange != null) {
+            CountingSource(originalSource, contentRange.start, contentRange.totalLength, channel)
+        } else {
+            CountingSource(originalSource, 0L, originalBody.contentLength(), channel)
+        }
     }
 
     private inline fun runAndCloseChannel(block: () -> Unit) {
