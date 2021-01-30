b0c878d0d184ea5c9ececddcfe57c9ed67b44fb6
radityagumay <gumay.raditya@gmail.com>
2021-01-30 19:14:28 +0700

    Avoid star import

--- okhttp/src/main/java/com/linecorp/lich/okhttp/OkHttpExtensions.kt
+++ okhttp/src/main/java/com/linecorp/lich/okhttp/OkHttpExtensions.kt
@@ -16,7 +16,11 @@
 package com.linecorp.lich.okhttp
 
 import kotlinx.coroutines.suspendCancellableCoroutine
-import okhttp3.*
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import java.io.IOException
 import kotlin.coroutines.resumeWithException
 
