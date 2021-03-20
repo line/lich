b5a9a416312dbb40d9f08088c08a12fd40edf14c
OISHI Masakuni <yamasa@bsdhouse.org>
2021-03-20 21:28:45 +0900

    Refactor ViewModelArgsProcessor.

---
+++ viewmodel-compiler/src/main/java/com/linecorp/lich/viewmodel/compiler/BundleMethodResolver.kt
@@ -0,0 +1,112 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linecorp.lich.viewmodel.compiler
+
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.ParameterizedTypeName
+import com.squareup.kotlinpoet.TypeName
+import com.squareup.kotlinpoet.WildcardTypeName
+
+internal object BundleMethodResolver {
+
+    fun resolvePutMethodName(
+        typeName: TypeName,
+        isOptional: Boolean,
+        isAssignableTo: (String) -> Boolean,
+        hasTypeArgumentAssignableTo: (String) -> Boolean
+    ): String? {
+        val rawTypeName = typeName.rawCanonicalName ?: return null
+
+        // For nullable primitive types, we cannot use put-methods such as
+        // "putInt(key: String, value: Int)". So, we use "putSerializable" instead.
+        if (!isOptional && typeName.isNullable && rawTypeName in primitiveTypes)
+            return "putSerializable"
+
+        rawTypeMethodMap[rawTypeName]?.let { return it }
+
+        parcelablesMethodMap[rawTypeName]?.let { methodName ->
+            if (hasTypeArgumentAssignableTo("android.os.Parcelable")) {
+                return methodName
+            } else if (rawTypeName != "android.util.SparseArray") {
+                // Any other "Array<T>" and "ArrayList<T>" can be put using "putSerializable".
+                return "putSerializable"
+            }
+        }
+
+        assignableTypeMethodMap.forEach { (superType, methodName) ->
+            if (isAssignableTo(superType)) return methodName
+        }
+
+        return null
+    }
+
+    val TypeName.rawCanonicalName: String?
+        get() = when (val erasure = if (this is WildcardTypeName) outTypes[0] else this) {
+            is ClassName -> erasure.canonicalName
+            is ParameterizedTypeName -> erasure.rawType.canonicalName
+            else -> null
+        }
+
+    private val primitiveTypes: Set<String> = setOf(
+        "kotlin.Byte",
+        "kotlin.Short",
+        "kotlin.Int",
+        "kotlin.Long",
+        "kotlin.Float",
+        "kotlin.Double",
+        "kotlin.Char",
+        "kotlin.Boolean"
+    )
+
+    private val rawTypeMethodMap: Map<String, String> = mapOf(
+        "kotlin.Byte" to "putByte",
+        "kotlin.Short" to "putShort",
+        "kotlin.Int" to "putInt",
+        "kotlin.Long" to "putLong",
+        "kotlin.Float" to "putFloat",
+        "kotlin.Double" to "putDouble",
+        "kotlin.Char" to "putChar",
+        "kotlin.Boolean" to "putBoolean",
+        "kotlin.ByteArray" to "putByteArray",
+        "kotlin.ShortArray" to "putShortArray",
+        "kotlin.IntArray" to "putIntArray",
+        "kotlin.LongArray" to "putLongArray",
+        "kotlin.FloatArray" to "putFloatArray",
+        "kotlin.DoubleArray" to "putDoubleArray",
+        "kotlin.CharArray" to "putCharArray",
+        "kotlin.BooleanArray" to "putBooleanArray",
+        "kotlin.String" to "putString",
+        "kotlin.CharSequence" to "putCharSequence",
+        "android.os.Bundle" to "putBundle",
+        "android.util.Size" to "putSize",
+        "android.util.SizeF" to "putSizeF"
+    )
+
+    private val parcelablesMethodMap: Map<String, String> = mapOf(
+        "kotlin.Array" to "putParcelableArray",
+        "kotlin.collections.ArrayList" to "putParcelableArrayList",
+        "java.util.ArrayList" to "putParcelableArrayList",
+        "android.util.SparseArray" to "putSparseParcelableArray"
+    )
+
+    private val assignableTypeMethodMap: Map<String, String> = mapOf(
+        "kotlin.CharSequence" to "putCharSequence",
+        "java.lang.CharSequence" to "putCharSequence",
+        "android.os.IBinder" to "putBinder",
+        "android.os.Parcelable" to "putParcelable",
+        "java.io.Serializable" to "putSerializable"
+    )
+}
---
+++ viewmodel-compiler/src/main/java/com/linecorp/lich/viewmodel/compiler/ViewModelArgsWriter.kt
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2021 LINE Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.linecorp.lich.viewmodel.compiler
+
+import com.squareup.kotlinpoet.ClassName
+import com.squareup.kotlinpoet.FileSpec
+import com.squareup.kotlinpoet.FunSpec
+import com.squareup.kotlinpoet.KModifier
+import com.squareup.kotlinpoet.ParameterSpec
+import com.squareup.kotlinpoet.PropertySpec
+import com.squareup.kotlinpoet.TypeSpec
+import javax.annotation.processing.Filer
+import javax.lang.model.element.Element
+
+internal object ViewModelArgsWriter {
+
+    fun writeToFiler(viewModelInfo: ViewModelInfo, originatingElement: Element, filer: Filer) {
+        viewModelInfo.toFileSpec(originatingElement).writeTo(filer)
+    }
+
+    private fun ViewModelInfo.toFileSpec(originatingElement: Element? = null): FileSpec {
+        val constructorSpec = FunSpec.constructorBuilder().apply {
+            arguments.forEach { addParameter(it.toConstructorParameterSpec()) }
+        }.build()
+
+        val propertySpecs = arguments.map { it.toPropertySpec() }
+
+        val toBundleFunSpec = FunSpec.builder("toBundle")
+            .addModifiers(KModifier.OVERRIDE)
+            .returns(bundleClass)
+            .beginControlFlow("return %T().apply", bundleClass).also { builder ->
+                arguments.forEach { it.addStatementTo(builder) }
+            }.endControlFlow()
+            .build()
+
+        val typeSpecBuilder = TypeSpec.classBuilder(argsClassName)
+            .addKdoc("A generated Args class for [%T].", viewModelClassName)
+            .addSuperinterface(viewModelArgsClass)
+            .primaryConstructor(constructorSpec)
+            .addProperties(propertySpecs)
+            .addFunction(toBundleFunSpec)
+        if (originatingElement != null) {
+            typeSpecBuilder.addOriginatingElement(originatingElement)
+        }
+
+        return FileSpec.get(argsClassName.packageName, typeSpecBuilder.build())
+    }
+
+    private fun ViewModelArgumentInfo.toConstructorParameterSpec(): ParameterSpec =
+        ParameterSpec.builder(name, parameterType).apply {
+            if (isOptional) defaultValue("%L", null)
+        }.build()
+
+    private fun ViewModelArgumentInfo.toPropertySpec(): PropertySpec =
+        PropertySpec.builder(name, parameterType)
+            .initializer("%N", name)
+            .build()
+
+    private fun ViewModelArgumentInfo.addStatementTo(builder: FunSpec.Builder) {
+        when {
+            putMethodName == null ->
+                builder.addStatement("TODO(%S)", "Cannot put `$name` to Bundle directly.")
+            isOptional ->
+                builder.addStatement("if (%N != null) %N(%S, %N)", name, putMethodName, name, name)
+            else ->
+                builder.addStatement("%N(%S, %N)", putMethodName, name, name)
+        }
+    }
+
+    private val viewModelArgsClass = ClassName("com.linecorp.lich.viewmodel", "ViewModelArgs")
+    private val bundleClass = ClassName("android.os", "Bundle")
+}
